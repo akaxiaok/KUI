23a820e83c408f755eddd43f2f64ce9bfdf1ede0
akaxiaok <akaxiaok@gmail.com>
2017-11-25 18:01:13 +0800

    config: update

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
-let path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
-  devtool: 'eval-source-map', // 生成 source-map 有多种类型
-  entry: path.resolve(__dirname + '/src/index.jsx'),//已多次提及的唯一入口文件 __dirname”是node.js中的一个全局变量，它指向当前执行脚本所在的目录
+  devtool: 'source-map', // 生成 source-map 有多种类型
+  entry: {
+    index: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000', path.resolve(__dirname, 'src/index.jsx')],
+    Test: path.resolve(__dirname, 'src/Test.jsx')
+  },//已多次提及的唯一入口文件 __dirname”是node.js中的一个全局变量，它指向当前执行脚本所在的目录
   output: {
-    path: path.resolve(__dirname + '/dist'),//打包后的文件存放的地方 // path.resolve 生成绝对路径
-    filename: 'app.js'//打包后输出文件的文件名
+    path: path.resolve(__dirname, 'dist'),//打包后的文件存放的地方 // path.resolve 生成绝对路径
+    filename: '[name].bundle.js',//打包后输出文件的文件名
+    publicPath: '/'
   },
+  plugins: [
+    new CleanWebpackPlugin(['dist']), // 清空 dist 目录
+    new HtmlWebpackPlugin({
+      title: 'Output'
+    }), // 生成 html 文件
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  // 改用 express & webpack-dev-middleware
   devServer: {
     contentBase: './dist', // webpackServer 使用的路径
     historyApiFallback: true, //不跳转
