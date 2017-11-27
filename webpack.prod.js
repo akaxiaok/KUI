d15eaa1e84807162d3e7cb28f69f85f7717e6fa7
akaxiaok <akaxiaok@gmail.com>
2017-11-27 21:34:16 +0800

    feature: set env as production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
@@ -11,6 +12,16 @@ module.exports = merge(common, {
   plugins: [
     new UglifyJSPlugin({  // 精简代码，丑化、压缩、裁剪不会运行的代码(tree shaking)，需要去掉 babel es6 模块，使用 webpack 的模块
       sourceMap: true
+    }),
+    // 设置环境变量
+    // new webpack.EnvironmentPlugin({
+    //   NODE_ENV: 'production', // use 'production' unless process.env.NODE_ENV is defined
+    //   DEBUG: false
+    // }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
     })
   ]
 });
